7399268dba07fa9091de73e75ddf0e61999f4c1c
camilagraciano-gif <camila.graciano@escola.pr.gov.br>
2026-03-05 14:17:34 +0000

    finalizar if-else e introdução arrays

---
+++ arrays/array-time.js
@@ -0,0 +1,13 @@
+// Um array é um rupo indexado de valores
+// Podem ser homogêneo / heterogeneos
+// Seus indices vão de "0"..."infinito" 
+// Possuem funçoes nativas (prontas)
+// Podem ser Iterado e Checados 
+
+const time = ['Peter', 'Pan', true, 36]
+    //indices   0         1     2     3
+
+console.log(time)
+console.log(time[1])
+console.log(time[32])
+
